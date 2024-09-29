3a0102900cfc4365cdb29bbca39e8a11aa820e1f
Prokhorov Kirill <mr.kirill.prokhorov@list.ru>
2024-09-29 20:14:32 +0300

    Add time service

---
+++ Time/main.cpp
@@ -0,0 +1,20 @@
+#include "timeservice.h"
+
+#include <QCoreApplication>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    TimeService timeService;
+
+    if (!QDBusConnection::sessionBus().registerService(TIME_SERVICE_NAME))
+        qDebug() << "Unable to register service: " << QDBusConnection::sessionBus().lastError().message();
+
+    if (!QDBusConnection::sessionBus().registerObject("/", &timeService, QDBusConnection::ExportAllSlots))
+        qDebug() << "Unable to register object: " << QDBusConnection::sessionBus().lastError().message();
+
+    qDebug() << "Service running...";
+
+    return a.exec();
+}
---
+++ Time/timeservice.h
@@ -0,0 +1,22 @@
+#ifndef TIMESERVICE_H
+#define TIMESERVICE_H
+
+#include "../definitions.h"
+
+#include <QObject>
+#include <QDBusConnectionInterface>
+#include <QDBusContext>
+
+class TimeService : public QObject, public QDBusContext
+{
+    Q_OBJECT
+    Q_CLASSINFO("D-Bus Interface", TIME_SERVICE_NAME)
+
+public:
+    explicit TimeService(QObject *parent = nullptr);
+
+public slots:
+    Q_SCRIPTABLE quint64 GetSystemTime() const;
+};
+
+#endif // TIMESERVICE_H
