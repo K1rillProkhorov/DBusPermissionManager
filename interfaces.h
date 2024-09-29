224c323a909f6ce8d7bdcaabe11f6bdd63787894
Prokhorov Kirill <mr.kirill.prokhorov@list.ru>
2024-09-29 20:13:23 +0300

    Add interfaces for permission and time services

---
+++ interfaces.h
@@ -0,0 +1,28 @@
+#ifndef INTERFACES_H
+#define INTERFACES_H
+
+#include "definitions.h"
+
+#include <QDBusAbstractInterface>
+
+class PermissionServiceInterface : public QDBusAbstractInterface
+{
+    Q_OBJECT
+
+public:
+    explicit PermissionServiceInterface(QObject *parent = nullptr)
+        : QDBusAbstractInterface(PERMISSION_SERVICE_NAME, "/",
+                                 PERMISSION_SERVICE_NAME, QDBusConnection::sessionBus(), parent) {}
+};
+
+class TimeServiceInterface : public QDBusAbstractInterface
+{
+    Q_OBJECT
+
+public:
+    explicit TimeServiceInterface(QObject *parent = nullptr)
+        : QDBusAbstractInterface(TIME_SERVICE_NAME, "/",
+                                 TIME_SERVICE_NAME, QDBusConnection::sessionBus(), parent) {}
+};
+
+#endif //INTERFACES_H
